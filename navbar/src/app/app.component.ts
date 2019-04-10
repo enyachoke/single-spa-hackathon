72a0cc4511d749adb572062ab863ad0e4f8a7695
Emmanuel Nyachoke <achachiez@gmail.com>
2019-04-10 17:53:43 +0300

    Check for auth

--- navbar/src/app/app.component.ts
+++ navbar/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'navbar-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'navbar';
+  constructor(private router: Router) { }
+  ngOnInit() {
+
+    if (!this.getAuth()) {
+      console.log('called');
+      this.router.navigate(['/login']);
+    } else {
+      this.router.navigateByUrl('/patientsearch');
+    }
+  }
+  getAuth() {
+    const auth = sessionStorage.getItem('auth.credentials');
+    return auth;
+  }
 }
